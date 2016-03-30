a43ff1ebda1a74f5a4e7b16d8a0b1474573332cc
Andre Luis da Costa Mendonca <andre.gasola.02@gmail.com>
2016-03-29 23:22:43 -0300

    First commit

---
+++ src/Node.java
@@ -0,0 +1,23 @@
+
+public class Node
+{
+	
+	int value;
+	Node previous;
+	Node next;
+	
+	public Node()
+	{
+		super();
+	}
+
+	public Node(int value, Node previous, Node next)
+	{
+		super();
+		
+		this.value = value;
+		this.previous = previous;
+		this.next = next;
+	}
+
+}
